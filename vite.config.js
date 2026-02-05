ae91512d7466d7ef69f7237cb4e83a39ec704be4
Amit Jaggi <amit@mopid.me>
2026-02-05 12:44:01 +0530

    vite.config file updated

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig(() => {
   const config = {
+    base: "/",
     plugins: [react(), svgr()],
     define: {},
     resolve: {
